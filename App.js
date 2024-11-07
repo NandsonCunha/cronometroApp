91e11f3ae2cb6db4d629a8234d77a28b05a00580
NandsonCunha <nandson.s.c@gmail.com>
2024-11-07 16:32:16 -0300

    first

--- App.js
+++ App.js
@@ -1,20 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { useState } from'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      time: 0
+    }
+
+    this.iniciaTime = this.iniciaTime.bind(this)
+
+  }
+
+  iniciaTime(){
+    setInterval(()=>{
+      this.setState({time:this.state.time + 1});
+    },1000)
+  }
+
+  paraTime(){
+    clearInterval();
+    this.setState({time:0});
+  }
+
+  render() {
+    return(
+      <View style={styles.main}>
+        <View style={styles.container}>
+          <Image style={styles.img} source={require("./assets/cronometro.png")}/>
+          <Text style={styles.cronom}>{this.state.time}</Text>
+        </View>
+
+
+        <View style={styles.btnView}>
+          <TouchableOpacity  style={styles.botao} onPress={this.iniciaTime}>
+            <View style={styles.btnArea}>
+              <Text>Parar</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao} onPress={this.state.time}>
+            <View style={styles.btnArea}>
+              <Text>Limpar</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        
+        <Text style={styles.textoCrono}>Ultimo tempo: 3.70s</Text>
+      
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main:{
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#add8e6',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  img:{
+    marginTop: 90,
+  },
+
+  cronom:{
+    marginTop:-150,
+    fontSize: 80,
+    color: '#FFF',
+    textAlign: 'center',
+    fontWeight:'bold',
+  },
+
+  container:{
+    flex:1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },  
+
+  botao:{
+    width: 130,
+    height:50,
+    borderWidth: 2,
+    borderRadius: 25,
+    borderColor: 'red', 
+    borderRadius: 15,
+    margin:20,
+  },
+
+  btnArea:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent: 'center',
+    alignItems:'center',
+  },
+
+  btnView:{
+    flex:1,
+    flexDirection:'row',
+    marginTop: 100,
+  },
+
+  textoCrono:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  }
+})
+
+
+export default App;
